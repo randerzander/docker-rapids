0e067d60826bc88925a1bb61a5930279503a082d
Randy Gelhausen <rgelhau@gmail.com>
2019-03-11 20:33:03 -0400

    Fixing rapidsai/xgboost branch

--- setup.sh
+++ setup.sh
@@ -20,6 +20,7 @@ git clone --recurse-submodules https://github.com/rapidsai/dask-xgboost
 
 git clone --recursive https://github.com/rapidsai/xgboost rapidsai-xgboost
 cd rapidsai-xgboost && git submodule update --init --recursive -- dmlc-core
+git checkout origin/cudf-interop
 
 #git clone --recursive https://github.com/dmlc/xgboost.git
 #git clone --recurse-submodules https://github.com/dmlc/xgboost
