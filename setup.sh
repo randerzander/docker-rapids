c57274421e48a7e4037a70638de5d984fc821f2b
Randy Gelhausen <rgelhau@gmail.com>
2019-03-12 20:02:07 -0400

    fixed setup.sh to pull directly from Keiths branch

--- setup.sh
+++ setup.sh
@@ -1,26 +1,21 @@
 wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
 
 git clone --recurse-submodules https://github.com/rapidsai/custrings
-git clone --recurse-submodules https://github.com/rapidsai/cudf
+#git clone --recurse-submodules https://github.com/rapidsai/cudf
+# cloning directly from strings WIP pr
+git clone --recurse-submodules https://github.com/kkraus14/cudf -b fea-ext-string-support
 
-cd cudf
-git remote add kkraus14 https://github.com/kkraus14/cudf
-git fetch kkraus14
-git checkout kkraus14/fea-ext-string-support
-cd ..
+git clone --recurse-submodules https://github.com/rapidsai/cuml -b branch-0.6
 
-git clone --recurse-submodules https://github.com/rapidsai/cuml
-
-git clone --recurse-submodules https://github.com/rapidsai/cugraph
+git clone --recurse-submodules https://github.com/rapidsai/cugraph -b branch-0.6
 
 git clone --recurse-submodules https://github.com/rapidsai/dask-cuda
 git clone --recurse-submodules https://github.com/rapidsai/dask-cudf
 
 git clone --recurse-submodules https://github.com/rapidsai/dask-xgboost
 
-git clone --recursive https://github.com/rapidsai/xgboost rapidsai-xgboost
+git clone --recursive https://github.com/rapidsai/xgboost -b cudf-interop rapidsai-xgboost
 cd rapidsai-xgboost && git submodule update --init --recursive -- dmlc-core
-git checkout origin/cudf-interop
 
 #git clone --recursive https://github.com/dmlc/xgboost.git
 #git clone --recurse-submodules https://github.com/dmlc/xgboost
